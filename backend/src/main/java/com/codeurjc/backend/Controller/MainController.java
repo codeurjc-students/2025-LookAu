a424e551e699a8f35affcfae590efded3e559eb9
Amanda Castro <a.castrol.2020@alumnos.urjc.es>
2025-03-01 03:20:18 +0100

    HTML - Groups Profile And Personal End
    Edit gruop ticket interface
    Personal ticket interface
    Edit personal ticket interfaceGroup profile
    Group profile
    Edit group profile
    Refactor names and folders

--- backend/src/main/java/com/codeurjc/backend/Controller/MainController.java
+++ backend/src/main/java/com/codeurjc/backend/Controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
     @GetMapping("/")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("groups-ticket");
+        modelAndView.setViewName("group-in");
         return modelAndView;
     }
 
